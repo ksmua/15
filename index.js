812ee56a9961ce094f2f4dffeb842675caac11fc
Serhii Kirilenko <ksmua85@gmail.com>
2020-08-09 18:24:48 +0300

    Cells can move

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-console.log(15);
-
 /* 
-**  Вимоги
-**  блоки виставляються в поле в рандомному порядку
-**  блок може рухатись якщо є вільне місце
-**  блок може штовхати інші блоки, група блоків може рухатись якщо є місце
+**  Вимоги:
+**  (+) блок може рухатись якщо є вільне місце
+**  (-) блоки виставляються в поле в рандомному порядку
+**  (-) блок може штовхати інші блоки, група блоків може рухатись якщо є місце
+**  (опція) можна задавати розмір поля в пікселях кратний 100
 */
-
 
 const gameArea = document.querySelector(".game-area");
 const test = document.querySelector(".win");
 const cellArr = [];
+const empty = {
+  row: 4,
+  col: 4
+}
+
+
+const createEl = (col, row, num) => {
+  const cell = document.createElement("div");
+
+  cell.classList = "cell";
+  cell.setAttribute("id", num);
+  cell.style.top = row * 100 - 100 + "px";
+  cell.style.left = col * 100 - 100 + "px";
+
+    const span = document.createElement("span");
+    
+    span.innerText = num;
+    span.classList = "noselect";
+
+  cell.appendChild(span);
+  cell.addEventListener('click', cellMove);
+
+  return cell;
+}
 
 
 const fill = (root) => {
   let fragment = document.createDocumentFragment();
   
-  const createEl = (col, row, num) => {
-    const cell = document.createElement("div");
-    
-    
-    cell.classList = "cell";
-    cell.setAttribute("id", num );
-    cell.style.top = row * 100 - 100 + "px";
-    cell.style.left = col * 100 - 100 + "px";
-    
-      const span = document.createElement("span");
-        span.innerText = num;
-
-    cell.appendChild(span);
-    cell.addEventListener('click', cellMove);
-    
-    return cell;
-  }
+  
 
   for (let row = 1; row <= 4; row++){
     for (let col = 1; col <= 4; col++){
@@ -40,9 +46,9 @@ const fill = (root) => {
         break;
       } else {
         const num = row * 4 - 4 + col;
-        console.log("col = ", col, " row = ", row, " num = ", num);
+        // console.log("col = ", col, " row = ", row, " num = ", num);
         fragment.appendChild( createEl(col, row, num) );
-        cellArr.push([row, col]);
+        cellArr.push([row, col]); // Save Cell curent position in Arr
       }
     }
   }
@@ -50,21 +56,38 @@ const fill = (root) => {
   root.appendChild(fragment);
 }
 
-const cellMove = (event) => {
-  console.log("event", event);
-  // let curentRow = cellArr[cell.id][0];
-  // let curentCol = cellArr[cell.id][1];
-};
-
-const empty = {
-  row: 4,
-  col: 4
+// if curent cell is neighbor with empty field 
+const canMove = (row, col) => {
+  return (
+    ( empty.row == row && Math.abs(empty.col - col) === 1 ) 
+    ||
+    ( empty.col == col && Math.abs(empty.row - row) === 1 ) );
 }
 
-// _ _ _ _[]
-// _ _ _ _
-// empty id = 16
+const cellMove = (event) => {
+  const currentCell = event.target.parentNode;
+  const id = currentCell.id - 1;
+  
+  // if curent cell is neighbor with empty field 
+  if (canMove(cellArr[id][0], cellArr[id][1])) {
+    // Move DOM Node to Empty field
+    currentCell.style.top = empty.row * 100 - 100 + "px";
+    currentCell.style.left = empty.col * 100 - 100 + "px";
 
+    // Save Cell curent position from Arr 
+    let curentRow = cellArr[id][0];
+    let curentCol = cellArr[id][1];
+    // Set new position for cell in Arr from empty field
+    cellArr[id][0] = empty.row;
+    cellArr[id][1] = empty.col;
+    // Set new position for empty field
+    empty.row = curentRow;
+    empty.col = curentCol;
+  
+  }
+  
+  
+};
 
 fill(gameArea);
 console.log("cellArr = ", cellArr);
